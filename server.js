132f1c72e767fa15fff2f1a0cc040dc30ecae0f8
johnhigginsavila <johnhiggins.avila@gmail.com>
2017-12-10 18:54:50 +0800

    [FEAT](CourseManagement)-#17

--- server.js
+++ server.js
@@ -8,18 +8,23 @@ const apiRoutes = require('./src/router');
 const morgan = require('morgan');
 const cors = require('cors');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use(morgan('combined'));
 app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:"application/vmd.api+json"}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({type:"application/vmd.api+json"}));
+
+
 
 apiRoutes(app);
 
