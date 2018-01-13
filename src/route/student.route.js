191ef8f434212c88be8f3b734017baef62052edb
johnhigginsavila <johnhiggins.avila@gmail.com>
2018-01-14 02:18:37 +0800

    [#7]FEAT(ClassDetail): Upload function

--- src/route/student.route.js
+++ src/route/student.route.js
@@ -5,6 +5,39 @@ const fs = require('fs');
 const multer = require('multer');
 const xlsxMiddleware = require('../middlewares/xlsx.middleware');
 
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, __dirname + '/uploads/')
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname)
+	}
+});
+
+const uploadXlsx = multer({
+	storage: storage,
+	fileFilter: function (req, file, callback) { //file filter
+		if (['xls', 'xlsx'].indexOf(file.originalname.split('.')[file.originalname.split('.').length - 1]) === -1) {
+			try {
+				fs.unlinkSync(__dirname + '/uploads/' + file.originalname);
+				console.log(result);
+				req.payload = result;
+				return callback(undefined);
+			}
+			catch (e) {
+				try {
+					fs.unlinkSync(__dirname + '/uploads/*');
+					console.log('ERROR FILE IS NOT DELETED!!!!');
+					return callback(undefined);
+				}
+				catch (e) { return callback(undefined); }
+			}
+		}
+		callback(null, true);
+	}
+});
+
+
 studentRouter.route('/all')
 .get(Student.getAllStudent);
 
@@ -15,8 +48,8 @@ studentRouter.route('/myClass/:myClassId')
 studentRouter.route('/program/:programId/')
 .get(Student.getStudentByProgram);
 
-// studentRouter.route('/bulk/:myClassId')
-// .post()
+studentRouter.route('/bulk/:programId/:myClassId')
+.post(uploadXlsx.single('student'), xlsxMiddleware.parseXLSX, Student.bulkCreateMyStudent);
 // .put()
 // .delete()
 
