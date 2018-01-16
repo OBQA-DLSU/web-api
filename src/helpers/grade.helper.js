032987f3a935a6cce830f2c7a7141a0e54ed27da
johnhigginsavila <johnhiggins.avila@gmail.com>
2018-01-17 03:44:43 +0800

    [#6]FEAT(ClassRecord): Grades

--- src/helpers/grade.helper.js
+++ src/helpers/grade.helper.js
@@ -34,3 +34,124 @@ exports.getGradeWithQueryObjectHelper = (operator, queryObjectArray) => {
     }
   });
 };
+
+exports.createUpdateGradeHelper = (
+  gradeData, // array that has programSopiId, assessmentId, and grade
+  term,
+  cycle,
+  academicYear,
+  studentId,
+  instructorId,
+  programCourseId,
+  myClassId
+) => {
+  return new Promise(async(resolve, reject) => {
+    let result, error = [], success = [];
+    try {
+      result = await Promise.all(gradeData.map(async(d) => {
+        let checkGrade, grade;
+        try {
+          checkGrade = await db.grade.findOne({where: {term, cycle, academicYear, studentId, instructorId, programCourseId, assessmentId: d.assessmentId, programSopiId: d.programSopiId}});
+          if (!checkGrade) {
+            newGrade = await createGradeFunction(d.grade, term, cycle, academicYear, studentId, instructorId, programCourseId, d.programSopiId, myClassId, d.assessmentId);
+          } else {
+            newGrade = await updateGradeFunction(checkGrade.id, d.grade);
+          }
+          success.push(newGrade);
+        }
+        catch(e) {
+          console.log(e);
+          error.push(e);
+        }
+      }));
+      resolve({error, success});
+    }
+    catch(e) {
+      reject(e);
+    }
+  });
+};
+
+// functions
+const createGradeFunction = (
+  grade,
+  term,
+  cycle,
+  academicYear,
+  studentId,
+  instructorId,
+  programCourseId,
+  programSopiId,
+  myClassId,
+  assessmentId ) => {
+  return new Promise(async(resolve, reject) => {
+    let newGrade, checkGrade;
+    try {
+      checkGrade = await db.grade.findOne({ where: {term, cycle, academicYear, studentId, instructorId, programCourseId, programSopiId, assessmentId} });
+      if (!checkGrade) {
+        newGrade = await db.grade.create({
+          grade,
+          term,
+          cycle,
+          academicYear,
+          studentId,
+          instructorId,
+          programCourseId,
+          programSopiId,
+          myClassId,
+          assessmentId
+        });
+      } else {
+        newGrade = null;
+      }
+      if (!newGrade) {
+        resolve(null);
+        return;
+      }
+      const gradeData = await db.grade.findOne({
+        where: {id: newGrade.id},
+        include: [
+          { model: db.student, include: [{ model: db.user, attributes: ['id','idNumber', 'email', 'lname', 'fname'] }] },
+          { model: db.instructor, include: [{ model: db.user, attributes: ['id','idNumber', 'email', 'lname', 'fname'] }] },
+          { model: db.programSopi, include: [{ model: db.sopi, include: [{ model: db.so }] }] },
+          { model: db.programCourse, include: [{ model: db.course }] },
+          { model: db.assessment },
+          { model: db.myClass }
+        ],
+        raw: true
+      })
+      resolve(gradeData);
+    }
+    catch (e) {
+      reject(e);
+    }
+  });
+};
+
+const updateGradeFunction = (id, grade) => {
+  return new Promise(async(resolve, reject) => {
+    let updatedGrade;
+    try {
+      updatedGrade = await db.grade.update({
+        grade
+      }, { where: {id}, individualHooks: true, returning: true });
+      if (!updatedGrade[1][0]) { resolve(null); return; }
+      const gradeData = await db.grade.findOne({
+        where: {id: updatedGrade[1][0].id},
+        include: [
+          { model: db.student, include: [{ model: db.user, attributes: ['id','idNumber', 'email', 'lname', 'fname'] }] },
+          { model: db.instructor, include: [{ model: db.user, attributes: ['id','idNumber', 'email', 'lname', 'fname'] }] },
+          { model: db.programSopi, include: [{ model: db.sopi, include: [{ model: db.so }] }] },
+          { model: db.programCourse, include: [{ model: db.course }] },
+          { model: db.assessment },
+          { model: db.myClass }
+        ],
+        raw: true
+      });
+      resolve(gradeData);
+    }
+    catch (e) {
+      reject(e);
+    }
+  });
+};
